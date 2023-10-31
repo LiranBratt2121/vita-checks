21889923286f621b14a0b1756a44b4e855bc99f9
liran bratt <brattlirannin@gmail.com>
2023-10-31 15:34:54 +0200

    Add styles

---
+++ Screens/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        // backgroundColor: '#ecf0f1',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        // color: '#3498db',
+    },
+    text: {
+        fontSize: 18,
+        marginBottom: 20,
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        borderWidth: 1,
+        margin: 10,
+        padding: 10,
+    },
+});
+
+export default styles;
